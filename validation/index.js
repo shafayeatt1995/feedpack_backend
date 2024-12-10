2fa199b23a54a7f9f65c95619a1629b4a26e5b5a
shafayeatt1995 <shafayetalanik@gmail.com>
2024-12-10 20:55:09 +0600

    webhook updated

--- validation/index.js
+++ validation/index.js
@@ -3,6 +3,7 @@ const { validationResult } = require("express-validator");
 const validation = function (req, res, next) {
   const errors = validationResult(req);
   const mappedErrors = errors.mapped();
+
   if (Object.keys(mappedErrors).length === 0) {
     next();
   } else {
